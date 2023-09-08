ecfa995a03e4a237a8db5414372984e8fd682a8c
João Pedro <joaopedrodiasde@gmail.com>
2023-09-08 15:56:54 -0300

    tirando as 2 pastas e colocando 2 pastas 1 html 1 js

--- dia 3/teste.js
+++ dia 3/teste.js
@@ -1,9 +1,31 @@
-let nome = "João Pedro"
-console.log(nome)
- nome = "João Pedro"
-console.log("---------aparencia------")
+let entredaDoUsuarioNome= prompt("Insira seu nome")
 
-let a = "JOAO PEDRO"
-console.log(a)
-a = "JOAO PEDRO"
+console.log(entredaDoUsuarioNome)
+
+let entredaDoUsuarioIdade = prompt("Insira sua idade")
+
+let anoAtual = 2023
+let idade = anoAtual - parseInt(entredaDoUsuarioIdade);
+
+console.log( entredaDoUsuarioIdade)
+
+
+let entredaDoUsuarioAltura = prompt("Insira sua Altura sem virgula")
+
+console.log(entredaDoUsuarioAltura)
+
+let entredaDoUsuarioPeso = prompt("Insira seu Peso")
+let entradaDoUsuariaPesoEmInt = parseInt(entredaDoUsuarioPeso)
+
+console.log(entredaDoUsuarioPeso)
+
+//Calculo de imc
+let imc = entredaDoUsuarioAltura / (parseFloat(entredaDoUsuarioPeso) * parseFloat(entredaDoUsuarioPeso)) 
+
+
+// mensagem 
+
+console.log(`Olá ${entredaDoUsuarioNome}, você tem ${entredaDoUsuarioIdade} anos de idade, nasceu em ${entredaDoUsuarioIdade - anoAtual}, 
+você tem ${entredaDoUsuarioAltura} de altura, e pesa ${entredaDoUsuarioPeso}kg e seuIMC é ${imc.toFixed(2)} Kg/m2 `)
+
 
